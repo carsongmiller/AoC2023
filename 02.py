d47a6b3fc9d52c1ac007768c783010b59f71f239
carsongmiller <carsongmiller@gmail.com>
2023-12-04 14:51:03 -0600

    Day 2 part 1 complete

---
+++ 02.py
@@ -0,0 +1,57 @@
+from timeit import default_timer as timer
+
+lines = str()
+with open('02.txt') as f:
+    # lines = [int(n) for n in f.readlines()]
+	lines = f.readlines()
+
+def part1(lines):
+	limitR = 12
+	limitG = 13
+	limitB = 14
+	gameIndex = 0
+	sum = 0
+
+	for game in lines:
+		game = game.strip()
+		gameID = int(game[5:game.find(":")])
+
+		trials = game[game.find(": ") + 2:len(game)].split("; ")
+		for trial in trials:
+			possible = True
+			colors = trial.split(", ")
+
+			for item in colors:
+				quant, color = item.split(" ")
+				quant = int(quant)
+				if ((color == "red" and quant > limitR) or 
+					(color == "green" and quant > limitG) or 
+					(color == "blue" and quant > limitB)):
+					possible = False
+					break
+
+			if not possible:
+				break
+			
+		if possible:
+			sum += gameID
+
+	return sum	
+
+def part2(lines):
+	return
+
+
+start = timer()
+p1 = part1(lines)
+end = timer()
+print("Part 1:", p1)
+print("Time (msec):", (end - start) * 1000)
+print()
+
+start = timer()
+p2 = part2(lines)
+end = timer()
+print("Part 2:", p2)
+print("Time (msec):", (end - start) * 1000)
+print()
